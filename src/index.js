8a718b8f687d7f9cd367e13db3cf4dbb2c3a03df
Yuliana Krupiak <night2505@gmail.com>
2021-03-12 20:05:38 +0200

    feat: update file
    change file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length===0){
+    return 0;
+  }
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length===0){
+    return 0;
+  }
+  let max = array[0]; 
+    for (let i = 0; i < array.length; i++) { 
+        if (max < array[i]) max = array[i]; 
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length===0){
+    return 0;
+  }
+  let average = 0;
+  for (let i = 0; i < array.length; i++) { 
+    average+=array[i]; 
+}
+return average/array.length;
 }
